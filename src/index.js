97ebf0ca7c61f30624fea876a796c5a8bbf391ea
Kamenko <a.kamenko98@mail.ru>
2018-02-23 15:26:30 +0300

    Finally

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+    var k = 0;
+    while ( number >= 5 ) {
+        k += Math.trunc(number / 5);
+        number = Math.trunc(number / 5);
+    }
+    return k;
 }
